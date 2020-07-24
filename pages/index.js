0019ecbea966b0f2687052c58a7c064373e687e2
shinya-ml <epsonia_ss08@icloud.com>
2020-07-24 21:07:16 +0900

    componentの雛形だけ作成

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import Buttons from '../components/Buttons';
+import WordList from '../components/WordList';
 
 class TopPage extends Component {
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <div>
+        <Header />
+        <Buttons />
+        <WordList />
+        <Footer />
+      </div>
+    );
   }
 }
 export default TopPage;
