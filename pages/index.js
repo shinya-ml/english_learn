b98801352ed52d0ca69dd2ab079e7627e1beffc6
shinya-ml <epsonia_ss08@icloud.com>
2020-07-25 01:48:27 +0900

    TopPageの雛形作成

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import Buttons from '../components/Buttons';
 import WordList from '../components/WordList';
+import Footer from '../components/Footer';
 
 class TopPage extends Component {
   render() {
