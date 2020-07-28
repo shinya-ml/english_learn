826e30edaeb67ef96d3538899ff8ef60175e87f8
shinya-ml <epsonia_ss08@icloud.com>
2020-07-29 00:04:47 +0900

    Header is rewritten by function component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Header from '../components/Header';
+import { Header } from '../components/Header';
 import Buttons from '../components/Buttons';
 import WordList from '../components/WordList';
 import WordForm from '../components/WordForm';
