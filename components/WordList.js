80bde779c246cbba85a8ea69a9578a264b13348c
shinya-ml <epsonia_ss08@icloud.com>
2020-08-02 20:53:24 +0900

    wordListをfunction componentに

--- components/WordList.js
+++ components/WordList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteWord } from '../ducks/slice';
 import NewWordInfoForm from './NewWordInfoForm';
 
 export const WordList = () => {
+  const wordList = useSelector((state) => state.word.wordList);
+  console.log(wordList);
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Word List</h1>
@@ -15,12 +18,14 @@ export const WordList = () => {
           </tr>
         </thead>
         <tbody>
-          {this.props.word.wordList.map((word, index) => (
+          {wordList.map((word, index) => (
             <tr key={index}>
               <td>{word.word}</td>
               <td>{word.ja}</td>
               <td>
-                <button onClick={() => this.onClick(index)}>delete</button>
+                <button onClick={() => dispatch(deleteWord(index))}>
+                  delete
+                </button>
               </td>
               <td>
                 <NewWordInfoForm index={index} />
@@ -32,46 +37,3 @@ export const WordList = () => {
     </div>
   );
 };
-
-class WordList extends Component {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-  }
-  onClick(index) {
-    console.log(index);
-    console.log(this.props);
-    this.props.deleteWord(index);
-  }
-  render() {
-    return (
-      <div>
-        <h1>Word List</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>English Word</th>
-              <th>Japanese Word</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.word.wordList.map((word, index) => (
-              <tr key={index}>
-                <td>{word.word}</td>
-                <td>{word.ja}</td>
-                <td>
-                  <button onClick={() => this.onClick(index)}>delete</button>
-                </td>
-                <td>
-                  <NewWordInfoForm index={index} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-const mapDispatchToProps = { deleteWord };
-export default connect((state) => state, mapDispatchToProps)(WordList);
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Header } from '../components/Header';
 import { Buttons } from '../components/Buttons';
-import WordList from '../components/WordList';
+import { WordList } from '../components/WordList';
 import WordForm from '../components/WordForm';
 import { Footer } from '../components/Footer';
 
