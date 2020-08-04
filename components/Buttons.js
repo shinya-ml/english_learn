c5e05ae319b5e1517e81ea76857b651a391df959
shinya-ml <epsonia_ss08@icloud.com>
2020-08-04 14:59:14 +0900

    ランダムにstoreにある英単語表示, ボタンを押したら選び直し

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Link from 'next/link';
 
 export const Buttons = () => {
   return (
     <div>
       <button>Button</button>
+      <Link href="/japanese_to_english_test">
+        <button>Test: Ja to En</button>
+      </Link>
     </div>
   );
 };
---
+++ components/JapaneseToEnglishTest.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+export const JapaneseToEnglishTest = () => {
+  const wordList = useSelector((state) => state.word.wordList);
+  const [showEnWord, setShowEnWord] = useState(wordList[0].word);
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+  const selectNextWord = () => {
+    const selectedIndex = getRandomInt(0, wordList.length);
+    setShowEnWord(wordList[selectedIndex].word);
+  };
+  return (
+    <div>
+      <h1>Test translating japanese to english</h1>
+      <p>{showEnWord}</p>
+      <button onClick={selectNextWord}>next word</button>
+    </div>
+  );
+};
