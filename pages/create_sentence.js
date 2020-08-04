e76962cf5bda788816e9488479b31f23d3ac11ee
shinya-ml <epsonia_ss08@icloud.com>
2020-08-04 23:10:54 +0900

    pageだけ作った

---
+++ pages/create_sentence.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+
+class CreateSentence extends Component {
+  render() {
+    <div>
+      <Header />
+      <h1>Create Setence</h1>
+      <Footer />
+    </div>;
+  }
+}
+export default CreateSentence;
