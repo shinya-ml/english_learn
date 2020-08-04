3340b9786fd2da7c872caf9ce6ff673485bd64f2
shinya-ml <epsonia_ss08@icloud.com>
2020-08-04 17:27:31 +0900

    add empty line

--- components/JapaneseToEnglishTest.js
+++ components/JapaneseToEnglishTest.js
@@ -5,15 +5,18 @@ import { useSelector } from 'react-redux';
 export const JapaneseToEnglishTest = () => {
   const wordList = useSelector((state) => state.word.wordList);
   const [showEnWord, setShowEnWord] = useState(wordList[0].word);
+
   const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   };
+
   const selectNextWord = () => {
     const selectedIndex = getRandomInt(0, wordList.length);
     setShowEnWord(wordList[selectedIndex].word);
   };
+
   return (
     <div>
       <h1>Test translating japanese to english</h1>
