27535e85dcd46b2d282ec28f2091101f24bd7df8
shinya-ml <epsonia_ss08@icloud.com>
2020-08-05 17:13:25 +0900

    header footerのcss作成

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { CardHeader } from '@material-ui/core';
 
 export const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <CardHeader title="Header" />
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import createStore from '../ducks/store';
 
+import '../css/Header.css';
+import '../css/Footer.css';
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={createStore()}>
